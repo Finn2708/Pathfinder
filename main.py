add5eab08d3d14695dac27c6837193988fc1324c
Finn <finn.weithoff@gmail.com>
2022-03-12 13:36:07 +0100

    Remove perf_counter for built-in pygame method

--- main.py
+++ main.py
@@ -1,33 +1,42 @@
 from src.data import DataGrid
 from src.interface import Interface
-from time import perf_counter, sleep
+from src.algorithms.astar import AStar
+from src.algorithms.dijkstra import Dijkstra
+import pygame.time
 
 
 if __name__ == '__main__':
+    # Add additional algorithms to this list:
+    algorithms = [Dijkstra, AStar]
+
+    # Set up the data container
     data = DataGrid()
+    # Start the user interface
     frontend = Interface(data)
 
     while not frontend.app_quit:
-        # Update data in generic data class
+        # Let the user draw a labyrinth
         frontend.handle(data)
+
+        # Space bar was pressed?
         if frontend.app_find_path:
-            # Choose algorithm
-            Algorithm = frontend.choose_algorithm()
-            algo = Algorithm()
-            algo.setup(data)
-            while not algo.done:
-                start = perf_counter()
+            # Choose the algorithm from algorithms list
+            Algorithm = frontend.choose_algorithm(algorithms)
+
+            # Dynamically create the algorithm object and set up any requirements
+            algo = Algorithm(data)
+            # algo.setup(data)
+
+            clock = pygame.time.Clock()
+            while not algo.is_done() and not frontend.app_quit:
                 algo.run_algorithm(data)
                 frontend.handle(data)
-                end = perf_counter()
-                # Render at 60 FPS
-                sleep_time = 1 / 60 - end + start
-                sleep(sleep_time if sleep_time > 0 else 0)
-            # Reconstruct the path
-            algo.show_path(data)
-            # Display the pass
-            frontend.handle(data)
-            frontend.wait_for_input()
+                clock.tick(240)
+
+            if algo.is_done():
+                # Display the result
+                frontend.handle(data)
+
             frontend.app_find_path = False
             frontend.reset_view(data)
             del algo
