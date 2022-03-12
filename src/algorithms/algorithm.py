6ef80bfa8bc52b51b512bf07cac855db4a9b3383
Finn <finn.weithoff@gmail.com>
2022-03-12 13:32:36 +0100

    Move general algorithm data to parent class

--- src/algorithms/algorithm.py
+++ src/algorithms/algorithm.py
@@ -1,14 +1,26 @@
 from abc import ABC, abstractmethod
 from src.data import DataGrid
+from src.data import Cell
 
 
 class Algorithm(ABC):
     """Abstract Base Class to implement strategy pattern for algorithms"""
-    done: bool
 
-    @abstractmethod
-    def setup(self, grid: DataGrid):
-        pass
+    # Name of the algorithm (displayed in the frontend)
+    name: str
+    # Current state of the algorithm
+    done: bool = False
+
+    # Start and goal cells
+    start: Cell
+    goal: Cell
+
+    def __init__(self, grid: DataGrid):
+        # Get the start and goal cells from the grid
+        sx, sy = grid.start_pos
+        gx, gy = grid.goal_pos
+        self.start = grid.cells[sx][sy]
+        self.goal = grid.cells[gx][gy]
 
     @abstractmethod
     def run_algorithm(self, grid: DataGrid):
@@ -17,3 +29,7 @@ class Algorithm(ABC):
     @abstractmethod
     def show_path(self, grid):
         pass
+
+    def is_done(self):
+        """Return true if the algorithm is done"""
+        return self.done
